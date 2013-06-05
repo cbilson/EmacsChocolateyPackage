72c378781858e3e99542f794c20022007dd4db9c
Joe Schafer <joe@jschaf.com>
2013-06-04 21:46:51 -0400

    Update Emacs to 24.3.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,2 +1,2 @@
-Install-ChocolateyZipPackage 'Emacs' 'http://ftp.gnu.org/pub/gnu/emacs/windows/emacs-24.2-bin-i386.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'Emacs' 'http://ftp.gnu.org/pub/gnu/emacs/windows/emacs-24.3-bin-i386.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
