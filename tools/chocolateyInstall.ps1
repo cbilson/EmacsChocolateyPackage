6fb61eadf6f3ff4934da8bc05d05372a6bfc37a5
Joe Schafer <joe@jschaf.com>
2013-06-04 21:47:12 -0400

    Add Emacs to the start menu.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,2 +1,3 @@
 Install-ChocolateyZipPackage 'Emacs' 'http://ftp.gnu.org/pub/gnu/emacs/windows/emacs-24.3-bin-i386.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+. $env:ChocolateyInstall\lib\Emacs.24.3\tools\emacs-24.3\bin\addpm.exe
