e429debad0739bf4895dc8b7a09b1ca4bd745860
Jonathan Clark <jon.h.clark@gmail.com>
2014-07-15 10:50:28 -0700

    Don't block the install with a modal dialog box.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,3 +1,4 @@
 Install-ChocolateyZipPackage 'Emacs' 'http://ftp.gnu.org/pub/gnu/emacs/windows/emacs-24.3-bin-i386.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-. $env:ChocolateyInstall\lib\Emacs.24.3\tools\emacs-24.3\bin\addpm.exe
+# Pass emacs install directory to addpm.exe so that it doesn't prompt with an OK modal dialog box
+. $env:ChocolateyInstall\lib\Emacs.24.3\tools\emacs-24.3\bin\addpm.exe "$env:ChocolateyInstall\lib\Emacs.24.3\tools\emacs-24.3"
