3bd1e807009c798ed6a2759ef1c3af8209e0608e
Chris Bilson <cbilson@pobox.com>
2014-07-21 05:54:49 -0700

    fix path to addpm.exe in tools dir

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,7 @@
-Install-ChocolateyZipPackage 'Emacs' 'http://ftp.gnu.org/pub/gnu/emacs/windows/emacs-24.3-bin-i386.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$emacsNameVersion = 'emacs-24.3'
+$src = "http://ftp.gnu.org/pub/gnu/emacs/windows/${emacsNameVersion}-bin-i386.zip'
+$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'Emacs' $src $toolsDir
 
 # Pass emacs install directory to addpm.exe so that it doesn't prompt with an OK modal dialog box
-. $env:ChocolateyInstall\lib\Emacs.24.3\tools\emacs-24.3\bin\addpm.exe "$env:ChocolateyInstall\lib\Emacs.24.3\tools\emacs-24.3"
+. $toolsDir\$emacsNameVersion\bin\addpm.exe $toolsDir\emacsNameVersion
