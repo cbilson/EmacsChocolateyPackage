5bb2e5deadee2e5de9063d7d4e678f9258fb4f27
Chris Bilson <cbilson@pobox.com>
2014-11-22 11:42:46 -0800

    upgrade to emacs 24.4

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,5 @@
-$emacsNameVersion = 'emacs-24.3'
-$src = "http://ftp.gnu.org/pub/gnu/emacs/windows/${emacsNameVersion}-bin-i386.zip'
-$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$emacsNameVersion = 'emacs-24.4'
+$src = "http://ftp.gnu.org/pub/gnu/emacs/windows/$($emacsNameVersion)-bin-i686-pc-mingw32.zip"
+$toolsDir =  Split-Path -parent $MyInvocation.MyCommand.Definition
 Install-ChocolateyZipPackage 'Emacs' $src $toolsDir
 
-# Pass emacs install directory to addpm.exe so that it doesn't prompt with an OK modal dialog box
-. $toolsDir\$emacsNameVersion\bin\addpm.exe $toolsDir\emacsNameVersion
