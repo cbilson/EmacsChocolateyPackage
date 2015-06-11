4e0d71b462b425b0f5c146fdd338f51355e8813a
Chris Bilson <cbilson@microsoft.com>
2015-06-11 07:59:16 -0700

    update to emacs 24.5

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-$emacsNameVersion = 'emacs-24.4'
-$src = "http://ftp.gnu.org/pub/gnu/emacs/windows/$($emacsNameVersion)-bin-i686-pc-mingw32.zip"
+$emacsNameVersion = 'emacs-24.5'
+$src = "http://ftp.gnu.org/pub/gnu/emacs/windows/$($emacsNameVersion)-bin-i686-mingw32.zip"
 $toolsDir =  Split-Path -parent $MyInvocation.MyCommand.Definition
 Install-ChocolateyZipPackage 'Emacs' $src $toolsDir
 
