a5f612fe679e7d0e0ff0d52f96d4f78d5dd78081
Chris Bilson <cbilson@microsoft.com>
2015-06-11 08:15:29 -0700

    add MD5 checksum

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,6 @@
 $emacsNameVersion = 'emacs-24.5'
 $src = "http://ftp.gnu.org/pub/gnu/emacs/windows/$($emacsNameVersion)-bin-i686-mingw32.zip"
 $toolsDir =  Split-Path -parent $MyInvocation.MyCommand.Definition
-Install-ChocolateyZipPackage 'Emacs' $src $toolsDir
-
+Install-ChocolateyZipPackage 'Emacs' $src $toolsDir `
+  -Checksum B867F51AC412B848D3CB17FDC8896040 `
+  -ChecksumType MD5
