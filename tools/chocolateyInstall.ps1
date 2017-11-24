dd81ef6d42ae65661a5895632942765c133f0e4d
Tim Schumacher <tim@datenknoten.me>
2017-11-24 18:49:07 +0100

    Update emacs to 25.3

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$emacsNameVersion = 'emacs-25.1'
-$url = "http://ftp.gnu.org/pub/gnu/emacs/windows/$($emacsNameVersion)-i686-w64-mingw32.zip"
-$url64 = "http://ftp.gnu.org/pub/gnu/emacs/windows/$($emacsNameVersion)-x86_64-w64-mingw32.zip"
+$emacsNameVersion = 'emacs-25.3'
+$url = "http://ftp.gnu.org/pub/gnu/emacs/windows/$($emacsNameVersion)-i686.zip"
+$url64 = "http://ftp.gnu.org/pub/gnu/emacs/windows/$($emacsNameVersion)_1-x86_64.zip"
 $toolsDir =  Split-Path -parent $MyInvocation.MyCommand.Definition
 Install-ChocolateyZipPackage 'Emacs' $url $toolsDir $url64 `
-  -Checksum e4874839e6d164e74b023f091cd2f4545aeeee190a895c91b49670e11dfadaf7 `
+  -Checksum d166faece3a3cb55fb9bd240724265649a0c599fac0ee0da2ebb0753d1b31ad9 `
   -ChecksumType 'SHA256' `
-  -Checksum64 dd64f517c58226ccc385b6d06124a581337a4420b7e519664ed93fbbc505f2f9 `
+  -Checksum64 716698025746cd38684a939702aacee6080064224cce517b055b8a182c693423 `
   -ChecksumType64 'SHA256'
