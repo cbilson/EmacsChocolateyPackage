c3090c1c8e8d8e4f76ba58a22644fe3757c7dc14
Tim Schumacher <tim@datenknoten.me>
2017-11-24 19:21:54 +0100

    Install emacs dependencies

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -7,3 +7,12 @@ Install-ChocolateyZipPackage 'Emacs' $url $toolsDir $url64 `
   -ChecksumType 'SHA256' `
   -Checksum64 716698025746cd38684a939702aacee6080064224cce517b055b8a182c693423 `
   -ChecksumType64 'SHA256'
+
+$emacsDepsVersion = 'emacs-25'
+$deps = "http://ftp.gnu.org/pub/gnu/emacs/windows/$($emacsDepsVersion)-i686-deps.zip"
+$deps64 = "http://ftp.gnu.org/pub/gnu/emacs/windows/$($emacsDepsVersion)-x86_64-deps.zip"
+Install-ChocolateyZipPackage 'Emacs' $deps $toolsDir $deps64 `
+  -Checksum 37daea32cc054ae6b709c015704bdf7b7459c63b1be27cdd590d2807e50b236a `
+  -ChecksumType 'SHA256' `
+  -Checksum64 d928e6caaeb5267f978dad0a54c90b0ea6f31ad384f43d8a6bb54c67d2b2f184 `
+  -ChecksumType64 'SHA256'
