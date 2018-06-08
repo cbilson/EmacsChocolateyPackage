c7669004158eea848a91de44e61ab08544cb5426
Kien Nguyen <kien.n.quang@gmail.com>
2018-06-09 04:17:19 +0900

    adding additional nuspec information

--- tools/install-lite.ps1
+++ tools/install-lite.ps1
@@ -1,14 +1,16 @@
 $packageName = 'emacs'
-$emacsNameVersion = 'emacs-26.1'
-$url = "https://ftp.gnu.org/pub/gnu/emacs/windows/$($emacsNameVersion)-i686-no-deps.zip"
-$url64 = "https://ftp.gnu.org/pub/gnu/emacs/windows/$($emacsNameVersion)-x86_64-no-deps.zip"
-$installDir =  $(Split-Path -parent $MyInvocation.MyCommand.Definition) + '/' + $packageName
+
+$majorVersion = '26'
+$minorVersion = '1'
+
+$emacsNameVersion = "$($packageName)-$($majorVersion).$($minorVersion)"
+$installDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition) + '/' + $packageName
 
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = $installDir
-    url            = $url
-    url64bit       = $url64
+    url            = "http://ftp.gnu.org/pub/gnu/emacs/windows/emacs-$($majorVersion)/$($emacsNameVersion)-i686.zip"
+    url64bit       = "http://ftp.gnu.org/pub/gnu/emacs/windows/emacs-$($majorVersion)/$($emacsNameVersion)-x86_64.zip"
     checksum       = '8aa09a1cc7714045fc92f3f21b738e4356481670c7a0546c7edccf92b41df0b3'
     checksumType   = 'sha256'
     checksum64     = 'e73febe3f3ce5918c871fa4195e2561599c425635f2100ddd1327eeb052a0d7d'
@@ -19,7 +21,7 @@ Install-ChocolateyZipPackage @packageArgs
 
 # Exclude executables from getting shim
 $guiExes = @("emacs.exe", "emacsclientw.exe")
-$shimExes = @("runemacs.exe")
+$shimExes = @("runemacs.exe", "emacsclient.exe")
 
 $guiFilter = "^" + $($guiExes -join "$|^") + "$"
 $shimFilter = "^" + $($shimExes -join "$|^") + "$"
