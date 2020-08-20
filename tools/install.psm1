6ca8de4e10bbceb765f0c782b6cec7e2e7d3bb9c
Tim Schumacher <tim@datenknoten.me>
2020-08-20 19:24:05 +0200

    feat: update to emacs 27.1

--- tools/install.psm1
+++ tools/install.psm1
@@ -1,7 +1,7 @@
 $packageName = 'emacs'
 
-$majorVersion = '26'
-$minorVersion = '3'
+$majorVersion = '27'
+$minorVersion = '1'
 
 $emacsNameVersion = "$($packageName)-$($majorVersion).$($minorVersion)"
 $installDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition) + '/' + $packageName
@@ -9,22 +9,22 @@ $installDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition) + '/' + $
 $FullPackageArgs = @{
     packageName    = $packageName
     unzipLocation  = $installDir
-    url            = "https://ftp.gnu.org/pub/gnu/emacs/windows/emacs-$($majorVersion)/$($emacsNameVersion)-i686.zip"
-    url64bit       = "https://ftp.gnu.org/pub/gnu/emacs/windows/emacs-$($majorVersion)/$($emacsNameVersion)-x86_64.zip"
-    checksum       = '77df3aa8edfcc248caf0ef8726460d08ad3feb8e21295183222981feb722be34'
+    url            = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-$($majorVersion)/$(emacsNameVersion)-i686.zip"
+    url64bit       = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-$($majorVersion)/$(emacsNameVersion)-x86_64.zip"
+    checksum       = 'bd99eae036b229e6105c070499c63994870c839c70404a12c04992e0c0fa0b5b'
     checksumType   = 'sha256'
-    checksum64     = '4cf4c41c77d3893a62ec8b88031680f63574b15b8658e614bcfc20ce0ef1ef1e'
+    checksum64     = '3b0b2e9355fccd07f67bc7272cabeaa6ae82f8f3cbe54008b611a331f61b4c7d'
     checksumType64 = 'sha256'
 }
 
 $NoDepsPackageArgs = @{
     packageName    = $packageName
     unzipLocation  = $installDir
-    url            = "https://ftp.gnu.org/pub/gnu/emacs/windows/emacs-$($majorVersion)/$($emacsNameVersion)-i686-no-deps.zip"
-    url64bit       = "https://ftp.gnu.org/pub/gnu/emacs/windows/emacs-$($majorVersion)/$($emacsNameVersion)-x86_64-no-deps.zip"
-    checksum       = '88d24adf0fa5e8bb72f3f7f15cb8b7f3f3bee541a0b194a6c3b9db172b8dbaba'
+    url            = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-$($majorVersion)/$(emacsNameVersion)-i686-no-deps.zip"
+    url64bit       = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-$($majorVersion)/$(emacsNameVersion)-x86_64-no-deps.zip"
+    checksum       = 'd9a5d3513f8aad075068db2f41a08676f50db900b62ce32c6edd54cbc7c54392'
     checksumType   = 'sha256'
-    checksum64     = '12d4d80a9c37baeacf8d6d808cde3173d6ca570ca684e69f0adf6ca66fa88bf6'
+    checksum64     = 'dc00a3bd152d984f079472d2fb34af6ae4beb9bfef56162d6aa4a6685571900a'
     checksumType64 = 'sha256'
 }
 
@@ -33,8 +33,8 @@ $emacsDepsVersion = "emacs-$($majorVersion)"
 $DepsPackageArgs = @{
   packageName    = $packageName
   unzipLocation  = $installDir
-  url            = "https://ftp.gnu.org/pub/gnu/emacs/windows/emacs-$($majorVersion)/$($emacsDepsVersion)-i686-deps.zip"
-  url64bit       = "https://ftp.gnu.org/pub/gnu/emacs/windows/emacs-$($majorVersion)/$($emacsDepsVersion)-x86_64-deps.zip"
+  url            = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-$($majorVersion)/$(emacsDepsVersion)-i686-deps.zip"
+  url64bit       = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-$($majorVersion)/$(emacsDepsVersion)-x86_64-deps.zip"
   checksum       = '4ff9057d4407f2d1149f158a2920acd09c31eff12a09ba25dc1776a3978f41ad'
   checksumType   = 'sha256'
   checksum64     = '3501e0c7f31cbf98ef7ac5cbbc8bae04ef4a72f52b00898965a8d702c4a5fae7'
